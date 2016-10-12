8d6334e2aa5522b817c9a62c9729f5e61fb689a7
Joseph Chingalo <profschingalo@gmail.com>
2016-10-12 11:46:47 +0300

    add menu as well as tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  application_url = 'http://127.0.0.1:8080/demo/';
+  orgunit: any;
+
+  //oraganisation unit configuration
+  tree_config = {
+    show_search : true,
+    multiple : false,
+    search_text : 'Search',
+    loading_message: 'Loading Organisation units...'
+  };
+
+  // set selected organisation unit
+  onSelected ( selected_orgunit ) {
+    this.orgunit = selected_orgunit;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,23 @@ import { routes } from './app.routes';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import {AboutComponent} from "./about/about.component";
+import {DhisMenuComponent} from "./dhis-menu/ng2-dhis-menu";
+import { TreeModule } from 'angular2-tree-component';
+import {DhisOrgUnitTreeComponent} from "./dhis-org-unit-tree/ng2-dhis-org-unit-tree";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    DhisMenuComponent,
+    DhisOrgUnitTreeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    TreeModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
